0e80b392b168d39386c50aac4392d9164af416f9
Tatur Georgy <qazwsxtf_1@mail.ru>
2022-07-11 14:36:49 +0300

    added 1st task

---
+++ example/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter two numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (b>a) 
+{
+    max = b;
+    min = a;
+}
+else
+{
+    min = b;
+    max = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
